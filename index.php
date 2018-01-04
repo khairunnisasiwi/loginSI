696856139c690732678d816eaff0df063be0104f
Siwi <siwinurkhairunnisa@gmail.com>
2018-01-04 13:43:08 +0700

    er

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+     <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/loginformstyle.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/hover.zoom.js"></script>
+    <script src="assets/js/hover.zoom.conf.js"></script>
+    <?php
+      include "connection.php";
+    ?>
+  </head>
+
+  <body>
     <?php
     	//Start session
     	session_start();	
@@ -48,3 +73,5 @@
       </tr>
       </table>
     </form>
+  </body>
+</html>
