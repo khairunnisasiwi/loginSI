18b6c58adac0757405f56714184ba6050ab55fe0
khairunnisasiwi <33446798+khairunnisasiwi@users.noreply.github.com>
2018-01-04 11:07:33 +0700

    Add files via upload

---
+++ loginsi.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 04, 2018 at 05:04 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `loginsi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `profileID` int(25) NOT NULL,
+  `profile_name` char(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profilerole`
+--
+
+CREATE TABLE `profilerole` (
+  `profileID` int(25) NOT NULL,
+  `role_id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(3) NOT NULL,
+  `rolename` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+CREATE TABLE `session` (
+  `sessionID` int(25) NOT NULL,
+  `userid` int(6) NOT NULL,
+  `logintime` date DEFAULT NULL,
+  `logouttime` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup`
+--
+
+CREATE TABLE `signup` (
+  `username` varchar(50) NOT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `role` int(1) NOT NULL,
+  `employee_number` int(5) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_number` varchar(12) NOT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `confirm_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userprofile`
+--
+
+CREATE TABLE `userprofile` (
+  `userid` int(6) NOT NULL,
+  `profileID` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `userid` int(6) NOT NULL,
+  `username` char(99) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `fullname` char(99) NOT NULL,
+  `emp_number` int(30) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `phone_number` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`profileID`);
+
+--
+-- Indexes for table `profilerole`
+--
+ALTER TABLE `profilerole`
+  ADD KEY `profileID` (`profileID`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `session`
+--
+ALTER TABLE `session`
+  ADD PRIMARY KEY (`sessionID`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `signup`
+--
+ALTER TABLE `signup`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `userprofile`
+--
+ALTER TABLE `userprofile`
+  ADD KEY `userid` (`userid`),
+  ADD KEY `profileID` (`profileID`);
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`userid`),
+  ADD KEY `user_table_username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `userid` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `profilerole`
+--
+ALTER TABLE `profilerole`
+  ADD CONSTRAINT `profilerole_ibfk_1` FOREIGN KEY (`profileID`) REFERENCES `profile` (`profileID`),
+  ADD CONSTRAINT `profilerole_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`);
+
+--
+-- Constraints for table `session`
+--
+ALTER TABLE `session`
+  ADD CONSTRAINT `session_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user_table` (`userid`);
+
+--
+-- Constraints for table `userprofile`
+--
+ALTER TABLE `userprofile`
+  ADD CONSTRAINT `userprofile_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user_table` (`userid`),
+  ADD CONSTRAINT `userprofile_ibfk_2` FOREIGN KEY (`profileID`) REFERENCES `profile` (`profileID`);
+
+--
+-- Constraints for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD CONSTRAINT `user_table_username` FOREIGN KEY (`username`) REFERENCES `signup` (`username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
