786aa8df514ee57e4284771303e31c2b9eb1b993
rossirahmayani <rossi.rahmayani.rr@gmail.com>
2018-01-06 16:47:14 +0700

    ubah koneksi sql

--- reg_act.php
+++ reg_act.php
@@ -1,4 +1,4 @@
-<?php include "db.php"; 
+<?php include "connection.php"; 
  $username = addslashes(strip_tags ($_POST['username']));
  $password = addslashes(strip_tags ($_POST['password'])); 
  $confirm = addslashes(strip_tags ($_POST['confirm'])); 
@@ -20,13 +20,13 @@ else {
     else {
     //untuk mengecek apakah form password dan form konfirmasi password sudah sama
         if ($password == $confirm){
-            $sql_get = mysql_query ("SELECT * FROM users WHERE username = '$username'");
-            $num_row = mysql_num_rows($sql_get);
+            $sql_get = mysqli_query ($bd, "SELECT * FROM users WHERE username = '$username'");
+            $num_row = mysqli_num_rows ($sql_get);
         //fungsi script ini adalah untuk mengecek ketersediaan username, jika tidak tersedia maka program akan berjalan
             if ($num_row ==0) {
                 $password = md5($password);
                 $confirm = md5($confirm);
-                $sql_insert = mysql_query("INSERT INTO users VALUES ('','$username','$password','$confirm','$fullname','$role','$employee_number','$email','$phonenumber','$address')");
+                $sql_insert = mysqli_query($bd, "INSERT INTO users VALUES ('','$username','$password','$confirm','$fullname','$role','$employee_number','$email','$phonenumber','$address')");
                 echo "Pendaftaran berhasil. Login <a href='index.php'>disini</a>";
             }
             else {
